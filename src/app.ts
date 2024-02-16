8d370d34335ce10aa7709abfeca285dc58fa563d
Soslan Aldatov <soslanaldatov@gmail.com>
2024-02-17 03:45:25 +0400

    add Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import Fastify from 'fastify';
 import * as ari from 'ari-client';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 import { config, ariUrl } from './config';
 import { logger } from './misc/Logger';
@@ -23,23 +25,70 @@ const fastify = Fastify({
       channel: client.Channel()
     };
 
-    fastify.post<{ Body: playbackCalloutBody }>(
-      '/api/v1/calls/playback-callout',
-      playbackCalloutOptions,
-      async function (request, reply) {
-        try {
-          const { numbers } = request.body;
-          const listOfNumbers: PhoneNumberWithSound[] = numbers.map(number => {
-            return { number, sound: 'demo-thanks' };
-          });
-          await CalloutService.initiateBulkPlaybackCalls(ariData, listOfNumbers);
-          reply.code(200).send({ message: 'Callout with playback started' });
-        } catch (err) {
-          logger.error(`Error in /api/calls/playback-callout: ${err}`);
-          reply.code(500).send({ error: 'An error occurred during processing.' });
-        }
+    fastify.register(fastifySwagger, {
+      openapi: {
+        info: {
+          title: 'Asterisk Callout API',
+          description: 'Asterisk Callout API Documentation',
+          version: '1.0.0'
+        },
+        servers: [
+          {
+            url: 'http://localhost:8080'
+          }
+        ],
+        components: {},
+        tags: [
+          {
+            name: 'Root',
+            description: 'Root endpoints'
+          }
+        ]
       }
-    );
+    });
+
+    fastify.register(fastifySwaggerUi, {
+      routePrefix: '/docs',
+      uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+      },
+      uiHooks: {
+        onRequest: function (_request, _reply, next) {
+          next();
+        },
+        preHandler: function (_request, _reply, next) {
+          next();
+        }
+      },
+      staticCSP: true,
+      transformStaticCSP: header => header,
+      transformSpecification: swaggerObject => {
+        return swaggerObject;
+      },
+      transformSpecificationClone: true
+    });
+
+    fastify.register((app, _options, done) => {
+      app.post<{ Body: playbackCalloutBody }>(
+        '/api/v1/calls/playback-callout',
+        playbackCalloutOptions,
+        async function (request, reply) {
+          try {
+            const { numbers } = request.body;
+            const listOfNumbers: PhoneNumberWithSound[] = numbers.map(number => {
+              return { number, sound: 'demo-thanks' };
+            });
+            await CalloutService.initiateBulkPlaybackCalls(ariData, listOfNumbers);
+            reply.code(200).send({ message: 'Callout with playback started' });
+          } catch (err) {
+            logger.error(`Error in /api/calls/playback-callout: ${err}`);
+            reply.code(500).send({ error: 'An error occurred during processing.' });
+          }
+        }
+      );
+      done();
+    });
 
     fastify.get('/health', function (_request, reply) {
       reply.send({ status: 'healthy' });
