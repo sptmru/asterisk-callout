e5d3d3ab62224e16d0d42efbe36bfa3a49a60c76
Soslan Aldatov <soslanaldatov@gmail.com>
2024-02-20 21:08:18 +0400

    move SipDriver enum to enums directory

--- src/entities/Extension.ts
+++ src/entities/Extension.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { SipDriver } from '../types/SipDriver';
+import { SipDriver } from '../types/enums/SipDriver';
 
 @Entity('extension')
 export class Extension {
