8a043a9b1e5adf0f4b6237bbdb1135ed30d52a0c
Soslan Aldatov <soslanaldatov@gmail.com>
2024-02-20 22:24:17 +0400

    reformat & refactor

--- src/migrations/1708449230731-create-extension-status-entity.ts
+++ src/migrations/1708449230731-create-extension-status-entity.ts
@@ -1,16 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateExtensionStatusEntity1708449230731 implements MigrationInterface {
-    name = 'CreateExtensionStatusEntity1708449230731'
+  name = 'CreateExtensionStatusEntity1708449230731';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."extension_status_status_enum" AS ENUM('available', 'busy')`);
-        await queryRunner.query(`CREATE TABLE "extension_status" ("id" SERIAL NOT NULL, "status" "public"."extension_status_status_enum" NOT NULL DEFAULT 'available', CONSTRAINT "PK_53112d6352d32408a7deafde572" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TYPE "public"."extension_status_status_enum" AS ENUM('available', 'busy')`);
+    await queryRunner.query(
+      `CREATE TABLE "extension_status" ("id" SERIAL NOT NULL, "status" "public"."extension_status_status_enum" NOT NULL DEFAULT 'available', CONSTRAINT "PK_53112d6352d32408a7deafde572" PRIMARY KEY ("id"))`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "extension_status"`);
-        await queryRunner.query(`DROP TYPE "public"."extension_status_status_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "extension_status"`);
+    await queryRunner.query(`DROP TYPE "public"."extension_status_status_enum"`);
+  }
 }
