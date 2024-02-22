49586baba0986d43c79d1a57840940e6c0aaa84d
Soslan Aldatov <soslanaldatov@gmail.com>
2024-02-22 23:50:49 +0400

    fix playback callout endpoint responses

--- src/controllers/callout/callout-playback.controller.ts
+++ src/controllers/callout/callout-playback.controller.ts
@@ -3,7 +3,6 @@ import { PhoneNumberWithSound } from '../../domain/types/phonenumberwithsound.ty
 import { CalloutService } from '../../services/CalloutService';
 import { logger } from '../../misc/Logger';
 import { playbackCalloutBody } from '../../domain/types/playbackcalloutbody.type';
-import { ErrorInterface, SuccessInterface } from '../../domain/interfaces/response.interface';
 
 export class CalloutPlaybackController {
   static async calloutPlayback(
@@ -16,12 +15,10 @@ export class CalloutPlaybackController {
         return { number, sound: 'demo-thanks' };
       });
       await CalloutService.initiateBulkPlaybackCalls(global.ariData, listOfNumbers);
-      const response: SuccessInterface = { success: { code: 200, message: 'Callout with playback started' } };
-      return reply.code(200).send(response);
+      return reply.code(200).send({ message: 'Callout with playback started' });
     } catch (err) {
       logger.error(`Error while handling playback callout: ${err}`);
-      const response: ErrorInterface = { error: { code: 500, message: 'Internal server error' } };
-      return reply.code(500).send(response);
+      return reply.code(500).send({ error: 'Internal server error' });
     }
   }
 }
